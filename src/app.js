89f3817acafd9ffc91ce34fa8751cb33219460fb
Quyết Nguyễn Hữu <huuquyet498@gmail.com>
2022-09-15 10:49:50 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,22 +1,53 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const userRouter = require("./routes/userRoutes");
 const roomRouter = require("./routes/roomRoutes");
 const messageRouter = require("./routes/messageRoutes");
+const helmet = require("helmet");
 const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 app.use(cors());
 
+app.options("*", cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set security HTTP headers
+app.use(helmet());
+
 // Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
 //Body parser, reading data from body into req.body
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
