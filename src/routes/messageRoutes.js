ce8b60d6b97c747395b72ebf2ea76428c46c13c3
Quyết Nguyễn Hữu <huuquyet498@gmail.com>
2022-09-17 17:06:56 +0700

    remove comment code and console.log

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const authController = require("../controller/authController");
-const messageController = require("../controller/messageController");
 const router = express.Router();
 
 router.use(authController.protect);
-
-router.route("/:roomId").get(messageController.getAllMessageInRoom);
 
 module.exports = router;
